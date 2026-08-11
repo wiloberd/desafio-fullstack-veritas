3d2d9bc3fc8df30a692db1f5cc79481a6572f7ee
wiloberd <wiloberd10@gmail.com>
2026-08-11 19:20:50 -0300

    feat(frontend): implementa interface inicial do kanban

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,142 @@
 import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
 
   return ( 
     <>
-      <h3>Processo Seletivo Estágio Full Stack - Desafio Técnico</h3>
-       <p>Mini Kanban de tarefas desenvolvido com React e Go como desafio técnico para processo seletivo da Veritas.</p>
+      <main className="app">
+        <header className="app-header">
+          <div className="kanban-header">
+              <div className="app-brand">
+                <span className="app-logo"><img src={logo} alt="Logo generica da aplicação" /></span>
+              </div>
+
+              <div className='kanban-title'>
+                <h1>Processo Seletivo Estágio Full Stack</h1>
+                <p>
+                  Desafio técnico — Mini Kanban de tarefas desenvolvido
+                  com React e Go.
+                </p>
+              </div>
+          </div>
+        </header>
+
+        <section className="kanban-board">
+          <div className="kanban-column">
+            <div className="column-header">
+              <div>
+                <h2>A Fazer</h2>
+                <p className="task-count">0 tarefas</p>
+              </div>
+            </div>
+
+             <div className="column-content">
+               <ul className='task-items'>
+                <li className="task-item">
+                  <div className="task-item-box">
+                      <div className="task-item-header-box">
+                          <h3>Tarefa 01</h3>
+                          <div className="task-item-actions">
+                            <button type="button" className="task-item-btn"
+                              aria-label="Editar tarefa">✏️</button>
+
+                            <button type="button" className="task-item-btn"
+                              aria-label="Excluir tarefa" > ❌ </button>
+                          </div>
+                      </div>
+                      <div className="task-item-description-box">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                          A facilis sint laudantium in, cum ad placeat enim repellat
+                          earum sapiente minima doloremque.</p>
+                      </div>
+                    </div>
+                </li>
+                <li className="task-item">
+                  <div className="task-item-box">
+                      <div className="task-item-header-box">
+                          <h3>Tarefa 02</h3>
+
+                          <div className="task-item-actions">
+                            <button type="button" className="task-item-btn"
+                              aria-label="Editar tarefa">✏️</button>
+
+                            <button type="button" className="task-item-btn"
+                              aria-label="Excluir tarefa" > ❌ </button>
+                          </div>
+                      </div>
+
+                      <div className="task-item-description-box">
+                        <p></p>
+                      </div>
+
+                    </div>
+                </li>
+                <li className="task-item">
+                  <div className="task-item-box">
+                      <div className="task-item-header-box">
+                          <h3>Tarefa 03</h3>
+
+                          <div className="task-item-actions">
+                            <button type="button" className="task-item-btn"
+                              aria-label="Editar tarefa">✏️</button>
+
+                            <button type="button" className="task-item-btn"
+                              aria-label="Excluir tarefa" > ❌ </button>
+                          </div>
+                      </div>
+
+                      <div className="task-item-description-box">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                          A facilis sint laudantium in, cum ad placeat enim repellat
+                          earum sapiente minima doloremque.</p>
+                      </div>
+
+                    </div>
+                </li>
+                <li className="task-item">
+                  <div className="task-item-box">
+                      <div className="task-item-header-box">
+                          <h3>Tarefa 04</h3>
+
+                          <div className="task-item-actions">
+                            <button type="button" className="task-item-btn"
+                              aria-label="Editar tarefa">✏️</button>
+
+                            <button type="button" className="task-item-btn"
+                              aria-label="Excluir tarefa" > ❌ </button>
+                          </div>
+                      </div>
+
+                      <div className="task-item-description-box">
+                        <p></p>
+                      </div>
+
+                    </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="kanban-column">
+            <div className="column-header">
+              <div>
+                <h2>Em Progresso</h2>
+                <p className="task-count">0 tarefas</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="kanban-column">
+            <div className="column-header">
+              <div>
+                <h2>Concluído</h2>
+                <p className="task-count">0 tarefas</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
