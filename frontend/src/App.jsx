9e133f2450b671d95f8bee22c0bd312578750eaa
wiloberd <wiloberd10@gmail.com>
2026-08-10 22:28:07 -0300

    chore: setup frontend with React and Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return ( 
+    <>
+      <h3>Processo Seletivo Estágio Full Stack - Desafio Técnico</h3>
+       <p>Mini Kanban de tarefas desenvolvido com React e Go como desafio técnico para processo seletivo da Veritas.</p>
+    </>
+  )
+}
+
+export default App
